fdc1fda65f1cce62074952262547365897bde1f7
Родин Юрий Александрович <derek.ass98@Mail.ru>
2023-03-30 19:53:04 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+
+int result = number / 10 % 10;
+
+// if (result < 0)
+// {
+//     result = result * -1;
+// }
+
+Console.WriteLine(result);
+
